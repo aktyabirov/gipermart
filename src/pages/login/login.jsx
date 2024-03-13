713ab4b0ee6e385bb1464ddbc56fb96a097710a9
Rahmatolloh Oktyabirov <rahmatollohoktyabirov@Rahmatollohs-MacBook-Pro-2.local>
2024-03-13 15:35:29 -0500

    login error

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/login', {
+    const response = await fetch('https://backend-62qa.onrender.com/login', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({ email: username, password }),
